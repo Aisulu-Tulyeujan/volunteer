6b772c5e93e5093d45d1c1afa20ff8981a40145d
rersoy1 <rersoy@cougarnet.uh.edu>
2025-09-25 23:55:17 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/login";
+import Register from "/components/register";
+import "./Style.css";
 
+function App(){
+  return (
+   <Router>
+     <div className="App">
+       <nav>
+         <ul>
+           <li>
+             <Link to="/">Login</Link>
+           </li>
+           <li>
+             <Link to="/register">Register</Link>
+           </li>
+         </ul>
+       </nav>
+
+       <Routes>
+         <Route path="/" element={<Login />} />
+         <Route path="/register" element="{<REgister />} />
+       </Routes>
+     </div>
+   </Router>
+ );
+}
+  
+    

aeaf02b4eef334896a5e5ef30454e12410f09bc8
rersoy1 <rersoy@cougarnet.uh.edu>
2025-09-25 23:38:43 -0500

    Update register.js

--- register.js
+++ register.js
@@ -1,1 +1,42 @@
+import React, { useState } from "react";
 
+function Register() {
+  const [email, setEmail] = useState(' ');
+  const [password, setPassword] = useState(' ');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Register email:", email, "password:" , password});
+  };
+
+  return {
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleRegister)>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input 
+          type="password"
+          placeholder="Enter to confirm password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+export default Register;

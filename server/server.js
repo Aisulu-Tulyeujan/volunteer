b0e9b481c7b7f5ff0afab92a9e43f787e86194a0
rersoy1 <rersoy@cougarnet.uh.edu>
2025-10-16 23:33:59 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const BodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
 
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(BodyParser.json());
+
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Node.js Auth Backend is Running!');
+});
+
+app.listen(PORT, () => {
+    console.log('Server listening on http://localhost:${PORT}');
+});

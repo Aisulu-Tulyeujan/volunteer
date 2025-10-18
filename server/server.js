d4d86c60159a1a86c81b24a96b67cc32924bd8e9
nvperez <noelanie.v.perez@icloud.com>
2025-10-17 21:58:47 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const BodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
+const volunteerRoutes = require('./routes/volunteerRoutes'); 
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ app.use(cors());
 app.use(BodyParser.json());
 
 app.use('/api/auth', authRoutes);
+
+app.use('/profiles', volunteerRoutes);
 
 app.get('/', (req, res) => {
     res.send('Node.js Auth Backend is Running!');

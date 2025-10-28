1584b347745d75357592b75f90fd36d38db5ee80
rersoy1 <rersoy@cougarnet.uh.edu>
2025-10-27 23:03:06 -0500

    Update UserCredentials.js with mangodb

--- server/models/UserCredentials.js
+++ server/models/UserCredentials.js
@@ -1,1 +1,38 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
+const userCredentialsSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userCredentialsSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+userCredentialsSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+}
+
+module.s = mongoose.model("UserCredentials", userCredentialsSchema);

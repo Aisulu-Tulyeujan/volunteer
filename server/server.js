8eb171c047cde767827eb35f3b247906459fa7bb
rersoy1 <rersoy@cougarnet.uh.edu>
2025-10-30 17:26:59 -0500

    Update server.js PORT 5050

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const BodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const volunteerRoutes = require('./routes/volunteerRoutes'); 
 const eventRoutes = require('./routes/eventRoutes');
@@ -10,33 +10,36 @@ const notificationRoutes = require('./routes/notificationRoutes');
 
 
 dotenv.config();
-console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
-
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 5050;
 
 app.use(cors());
-app.use(BodyParser.json());
 
-// connect mongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected sucessfully "))
-  .catch(err => console.error("MongoDB connection error:", err));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 
 app.use('/api/auth', authRoutes);
-
-app.use('/api/profiles', volunteerRoutes);
-
-
-app.use('/api/events', eventRoutes);
-app.use('/api/notifications', notificationRoutes);
+app.use('/profiles', volunteerRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 
 app.get('/', (req, res) => {
     res.send('Node.js Auth Backend is Running!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
-});
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("MongoDB connected successfully");
+        app.listen(PORT, () => {
+            console.log(`Server running successfully on port ${PORT}`);
+            console.log(`Use URL: http://localhost:${PORT}/api/auth/register`);
+        });
+    })
+    .catch(err => {
+        console.error("MongoDB connection error:", err.message);
+        console.error("Please check your MONGODB_URI and Atlas IP Access.");
+
+    });

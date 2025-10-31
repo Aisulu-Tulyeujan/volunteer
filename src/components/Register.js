0b7bfe21dd2382b55d0fe557158620a29dccd042
rersoy1 <rersoy@cougarnet.uh.edu>
2025-10-31 00:26:42 -0500

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; 
+import { Link, useNavigate } from "react-router-dom"; 
 
 export default function Register() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+   const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
@@ -15,13 +18,39 @@ export default function Register() {
       return;
     }
 
-    console.log("Register email:", email, "password:", password);
+    try {
+      const response = await fetch("http://localhost:5050/api/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, password }),
+      });
+
+      const data = await response.json();
+      console.log("Register response:", data);
+
+      if (!response.ok) {
+        throw new Error(data.message || "Registration failed");
+      }
+
+      navigate("/", {replace: true });
+
+    } catch (err) {
+        console.error("Login error:", err);
+        setError(err.message);
+      }
   };
 
   return (
     <div className="auth-page">
       <h2>Register</h2>
       <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter full name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
         <input
           type="email"
           placeholder="Enter email"
@@ -45,6 +74,8 @@ export default function Register() {
         />
         <button type="submit">Register</button>
       </form>
+
+      {error && <p style={{ color: "red", marginTop: "10px" }}>{error}</p>}
 
       <p className="switch-text">
         Already have an account?

b1f2bb1234f024aaf1c510b95ba1f5915f74d89a
rersoy1 <rersoy@cougarnet.uh.edu>
2025-10-30 19:57:13 -0500

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const router = express.Router();
+const auth = require("../middleware/authMiddleware");
+const admin = require("../middleware/adminMiddleware");
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+router.get("/profile", auth, (req, res) => {
+    res.json({ message: "Welcome to your profile!", user: req.user });
+});
+router.get("/admin-only", auth, admin, (req, res) => {
+    res.json({ message: "Welcome Admin" });
+});
 
 module.exports = router;
